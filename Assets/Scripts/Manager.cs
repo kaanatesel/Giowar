0dc0aeb9631401452ce7b07335ceb1e45b58737c
kaanatesel <kaanatesel99@gmail.com>
2020-08-20 01:39:48 +0300

    select manager is done

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngineInternal;
 
 public class Manager : MonoBehaviour
@@ -25,16 +26,33 @@ public class Manager : MonoBehaviour
             Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
             RaycastHit2D hit = Physics2D.Raycast(pos, Camera.main.transform.forward);
 
+            if (activeObject != null)
+            {
+                
+                SpriteRenderer spriteRenderer = activeObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+                spriteRenderer.color = Color.red;
+            }
+
             if (hit.collider != null)
             {
+               
                 if (hit.collider.tag == "Platform")
                 {
-                    Debug.Log("hit platform");
-                    SpriteRenderer spriteRenderer = hit.collider.gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+                    Debug.Log("this is Platform=");
+                    activeObject = hit.collider.gameObject;
+                    SpriteRenderer spriteRenderer = activeObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
                     spriteRenderer.color = Color.black;
                 }
                 if (hit.collider.tag == "Factory")
+                {
                     Debug.Log("this is Factory");
+                }
+            }
+            else
+            {
+                SpriteRenderer spriteRenderer = activeObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+                spriteRenderer.color = Color.red;
+                activeObject = null;
             }
         }
     }
--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -1,9 +1,11 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 public class PlatformScript : MonoBehaviour
 {
     private const float radius = 60f;
+    private bool acvtive = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,5 +15,24 @@ public class PlatformScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+/*        if(acvtive)
+        {
+            Debug.Log("active plat");
+        }
+        else
+        {
+
+        }*/
     }
+
+/*
+    public void setActive(bool boolean)
+    {
+        acvtive = boolean;
+    }
+
+    bool isActive()
+    {
+        return acvtive;
+    }*/
 }
