6fc76cf75009f4755d684fd3430a873e6711ff82
kaanatesel <kaanatesel99@gmail.com>
2020-08-26 04:28:26 +0300

    summon new build by building panel

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public Manager manager;
+    public void openBuildPanel()
+    {
+        manager.buildPanel.SetActive(true);
+    }
+    public void closeBuildPanel()
+    {
+        manager.buildPanel.SetActive(false);
+    }
+    public void setWillBuildObject(GameObject obj)
+    {
+        manager.willBuildObject = obj;
+        manager.setBuildState(true);
+        manager.buildPanel.SetActive(false);
+    }
+}
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngineInternal;
 
 public class Manager : MonoBehaviour
 {
+    //Public Variables
     public Button buildBtn;
+    public GameObject buildPanel;
+    public GameObject willBuildObject;
+    //Private Variables
     private GameObject activeObject = null;
+    private bool buildStateActive = false;
     void Start()
     {
         
@@ -34,39 +40,87 @@ public class Manager : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             Vector2 pos = Camera.main.ScreenToWorldPoint(touch.position);
             RaycastHit2D hit = Physics2D.Raycast(pos, Camera.main.transform.forward);
-            if (touch.position.y > 30)
+
+            if(EventSystem.current.IsPointerOverGameObject())
             {
-                if (hit.collider != null)
-                {
-                    if (activeObject != null)
-                    {
-                        IselectAble tmp = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
-                        tmp.setSelect(false);
-                        activeObject = hit.collider.gameObject;
-                        IselectAble selectedObject = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
-                        selectedObject.setSelect(true);
-                    }
-                    else
-                    {
-                        activeObject = hit.collider.gameObject;
-                        IselectAble selectedObject = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
-                        selectedObject.setSelect(true);
-                    }
-                }
-                else
-                {
-                    if (activeObject != null)
-                    {
-                        IselectAble platform = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
-                        platform.setSelect(false);
-                        activeObject = null;
-                    }
-                }
+                Debug.Log("on game object");
             }
             else
             {
-                // menu element
+                if (buildPanel.activeSelf)
+                {
+                    buildPanel.SetActive(false);
+                }
+                else if(buildStateActive)
+                {
+                    
+                    if (Input.touchCount > 0)
+                    {
+                        float distance = Vector2.Distance(activeObject.transform.position, touch.position);
+                        Debug.Log(distance);
+                        if (touch.phase == TouchPhase.Began
+                           && willBuildObject != null
+                           && touch.position.y > 30
+                           && hit.collider == null
+                           && distance <= 330
+                           )
+                        {
+                            Instantiate(willBuildObject, pos, Quaternion.identity);
+                        }
+                        buildStateActive = false;
+                    }
+                }
+                else {
+                    if (touch.position.y > 30)
+                    {
+                        if (hit.collider != null)
+                        {
+                            if (activeObject != null)
+                            {
+                                IselectAble tmp = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
+                                tmp.setSelect(false);
+                                activeObject = hit.collider.gameObject;
+                                IselectAble selectedObject = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
+                                selectedObject.setSelect(true);
+                            }
+                            else
+                            {
+                                activeObject = hit.collider.gameObject;
+                                IselectAble selectedObject = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
+                                selectedObject.setSelect(true);
+                            }
+                        }
+                        else
+                        {
+                            if (activeObject != null)
+                            {
+                                IselectAble platform = activeObject.GetComponent(typeof(IselectAble)) as IselectAble;
+                                platform.setSelect(false);
+                                activeObject = null;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // menu element
+                    }
+                }
             }
         }
+    }
+
+    public void setBuildState(bool boolean)
+    {
+        buildStateActive = boolean;
+    }
+
+    public bool isBuildStateActive()
+    {
+        return buildStateActive;
+    }
+
+    public void endBuildingState()
+    {
+        setBuildState(false);
     }
 }
